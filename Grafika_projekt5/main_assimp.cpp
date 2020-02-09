1203f876cf1c7382f2e16417cda2bc6afaecb4de
JakubBrojacz <38508437+JakubBrojacz@users.noreply.github.com>
2020-02-09 21:12:53 +0100

    Remove unnecessary shaders & lock camera when not in V mode

--- Grafika_projekt5/main_assimp.cpp
+++ Grafika_projekt5/main_assimp.cpp
@@ -35,6 +35,7 @@ Camera camera(glm::vec3(30, 30, 30));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+bool cameraLocked = false;
 
 // timing
 float deltaTime = 0.0f;
@@ -44,10 +45,8 @@ float lastFrame = 0.0f;
 Fog fog(5, 10, 20);
 DayNight sun(10);
 
-// current shader
-std::shared_ptr<Shader> currentShader = nullptr;
-std::shared_ptr<Shader> phongShader = nullptr;
-std::shared_ptr<Shader> blinnShader = nullptr;
+// shader
+std::shared_ptr<Shader> mainShader;
 
 enum class CameraMode { Free, Car, Stationary, Following };
 
@@ -95,11 +94,9 @@ int main()
 
 	
 	// init shader params
-	phongShader = std::make_shared<Shader>("phong.vs", "phong.fs");
-	blinnShader = std::make_shared<Shader>("blinn.vs", "blinn.fs");
-	currentShader = blinnShader;
-	currentShader->use();
-	currentShader->setInt("mode", 0);
+	mainShader = std::make_shared<Shader>("blinn.vs", "blinn.fs");
+	mainShader->use();
+	mainShader->setInt("mode", 0);
 
 	// load models
 	// -----------
@@ -159,7 +156,7 @@ int main()
 		{
 		case CameraMode::Car:
 			camera.Position = car_light_pos;
-			//camera.Front = car_light_direction;
+			camera.Front = car_light_direction;
 			camera.Up = glm::vec3{ 0, 1, 0 };
 			break;
 		case CameraMode::Stationary:
@@ -177,50 +174,50 @@ int main()
 		// 2. render scene as normal using the generated depth/shadow map  
 		// --------------------------------------------------------------
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-		currentShader->use();
+		mainShader->use();
 
 		// render the loaded model
-		currentShader->setMat4("ProjectionMatrix", projection);
-		currentShader->setVec3("ViewPosition", camera.Position);
+		mainShader->setMat4("ProjectionMatrix", projection);
+		mainShader->setVec3("ViewPosition", camera.Position);
 		
-		currentShader->setFloat("FogStart", fog.start);
-		currentShader->setFloat("FogEnd", fog.end);
-		currentShader->setFloat("FogIntensity", fog.intensity);\
+		mainShader->setFloat("FogStart", fog.start);
+		mainShader->setFloat("FogEnd", fog.end);
+		mainShader->setFloat("FogIntensity", fog.intensity);\
 
-		currentShader->setVec3("Sun.intensity", sun.Color());
-		currentShader->setVec3("Sun.direction", sun.Direction());
+		mainShader->setVec3("Sun.intensity", sun.Color());
+		mainShader->setVec3("Sun.direction", sun.Direction());
 		
-		currentShader->setVec4("Spot[0].position", glm::vec4{ 17, 26, -0.75f, 1});
-		currentShader->setVec3("Spot[0].intensity", 0.0f, 4.0f, 0.0f);
-		currentShader->setVec3("Spot[0].direction", glm::vec3{ 0, -1, 0 });
-		currentShader->setFloat("Spot[0].exponent", 0.1);
-		currentShader->setFloat("Spot[0].cutoff", 30);
+		mainShader->setVec4("Spot[0].position", glm::vec4{ 17, 26, -0.75f, 1});
+		mainShader->setVec3("Spot[0].intensity", 0.0f, 4.0f, 0.0f);
+		mainShader->setVec3("Spot[0].direction", glm::vec3{ 0, -1, 0 });
+		mainShader->setFloat("Spot[0].exponent", 0.1);
+		mainShader->setFloat("Spot[0].cutoff", 30);
 
-		currentShader->setVec4("Spot[1].position", glm::vec4{ -19, 26, -0.75f, 1 });
-		currentShader->setVec3("Spot[1].intensity", 0.0f, 0.0f, 4.0f);
-		currentShader->setVec3("Spot[1].direction", glm::vec3{ 0, -1, 0 });
-		currentShader->setFloat("Spot[1].exponent", 0.1);
-		currentShader->setFloat("Spot[1].cutoff", 30);
+		mainShader->setVec4("Spot[1].position", glm::vec4{ -19, 26, -0.75f, 1 });
+		mainShader->setVec3("Spot[1].intensity", 0.0f, 0.0f, 4.0f);
+		mainShader->setVec3("Spot[1].direction", glm::vec3{ 0, -1, 0 });
+		mainShader->setFloat("Spot[1].exponent", 0.1);
+		mainShader->setFloat("Spot[1].cutoff", 30);
 
-		currentShader->setVec4("Spot[2].position", glm::vec4(car_light_pos, 1));
-		currentShader->setVec3("Spot[2].intensity", 4.0f, 4.0f, 4.0f);
-		currentShader->setVec3("Spot[2].direction", car_light_direction);
-		currentShader->setFloat("Spot[2].exponent", 0.1);
-		currentShader->setFloat("Spot[2].cutoff", 30);
+		mainShader->setVec4("Spot[2].position", glm::vec4(car_light_pos, 1));
+		mainShader->setVec3("Spot[2].intensity", 4.0f, 4.0f, 4.0f);
+		mainShader->setVec3("Spot[2].direction", car_light_direction);
+		mainShader->setFloat("Spot[2].exponent", 0.1);
+		mainShader->setFloat("Spot[2].cutoff", 30);
 		
-		currentShader->setVec3("Kd", 0.4, 0.4, 0.4);
-		currentShader->setVec3("Ka", 0.3, 0.3, 0.3);
-		currentShader->setVec3("Ks", 0.6, 0.6, 0.6);
-		currentShader->setFloat("Shininess", 0.1);
+		mainShader->setVec3("Kd", 0.4, 0.4, 0.4);
+		mainShader->setVec3("Ka", 0.3, 0.3, 0.3);
+		mainShader->setVec3("Ks", 0.6, 0.6, 0.6);
+		mainShader->setFloat("Shininess", 0.1);
 
-		currentShader->setMat4("ModelViewMatrix", sponza_model);
-		currentShader->setMat3("NormalMatrix", glm::transpose(glm::inverse(glm::mat3(sponza_model))));
-		currentShader->setMat4("MVP", projection * view * sponza_model);
-		Sponza.Draw(*currentShader);
-		currentShader->setMat4("ModelViewMatrix", car_model);
-		currentShader->setMat3("NormalMatrix", glm::transpose(glm::inverse(glm::mat3(car_model))));
-		currentShader->setMat4("MVP", projection * view * car_model);
-		ridingCar.Draw(*currentShader);
+		mainShader->setMat4("ModelViewMatrix", sponza_model);
+		mainShader->setMat3("NormalMatrix", glm::transpose(glm::inverse(glm::mat3(sponza_model))));
+		mainShader->setMat4("MVP", projection * view * sponza_model);
+		Sponza.Draw(*mainShader);
+		mainShader->setMat4("ModelViewMatrix", car_model);
+		mainShader->setMat3("NormalMatrix", glm::transpose(glm::inverse(glm::mat3(car_model))));
+		mainShader->setMat4("MVP", projection * view * car_model);
+		ridingCar.Draw(*mainShader);
 
 
 		// glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
@@ -242,14 +239,18 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
 		glfwSetWindowShouldClose(window, true);
 
-	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
-		camera.ProcessKeyboard(FORWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
-		camera.ProcessKeyboard(BACKWARD, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
-		camera.ProcessKeyboard(LEFT, deltaTime);
-	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
-		camera.ProcessKeyboard(RIGHT, deltaTime);
+	if (!cameraLocked)
+	{
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			camera.ProcessKeyboard(FORWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			camera.ProcessKeyboard(BACKWARD, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			camera.ProcessKeyboard(LEFT, deltaTime);
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			camera.ProcessKeyboard(RIGHT, deltaTime);
+	}
+	
 
 	if (glfwGetKey(window, GLFW_KEY_Q) == GLFW_PRESS)
 	{
@@ -257,28 +258,31 @@ void processInput(GLFWwindow* window)
 		std::cout << "{" << camera.Front.x << ", " << camera.Front.y << ", " << camera.Front.z << "}" << std::endl;
 		std::cout << "{" << camera.Up.x << ", " << camera.Up.y << ", " << camera.Up.z << "}" << std::endl;
 		std::cout << "deltatime: " << deltaTime << std::endl;
-
 	}
 
 	if (glfwGetKey(window, GLFW_KEY_Z) == GLFW_PRESS)
 	{
 		std::cout << "Camera attached to car" << std::endl;
 		cameraMode = CameraMode::Car;
+		cameraLocked = true;
 	}
 	if (glfwGetKey(window, GLFW_KEY_X) == GLFW_PRESS)
 	{
 		std::cout << "Camera stationary" << std::endl;
 		cameraMode = CameraMode::Stationary;
+		cameraLocked = true;
 	}
 	if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS)
 	{
 		std::cout << "Camera following car" << std::endl;
 		cameraMode = CameraMode::Following;
+		cameraLocked = true;
 	}
 	if (glfwGetKey(window, GLFW_KEY_V) == GLFW_PRESS)
 	{
 		std::cout << "Camera free /debug mode/" << std::endl;
 		cameraMode = CameraMode::Free;
+		cameraLocked = false;
 	}
 	if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS)
 	{
@@ -293,14 +297,12 @@ void processInput(GLFWwindow* window)
 	if (glfwGetKey(window, GLFW_KEY_B) == GLFW_PRESS)
 	{
 		std::cout << "Activate Blinn lightning" << std::endl;
-		currentShader->setInt("mode", 0);
-		//currentShader = blinnShader;
+		mainShader->setInt("mode", 0);
 	}
 	if (glfwGetKey(window, GLFW_KEY_N) == GLFW_PRESS)
 	{
 		std::cout << "Activate Phong lightning" << std::endl;
-		currentShader->setInt("mode", 1);
-		//currentShader = phongShader;
+		mainShader->setInt("mode", 1);
 	}
 }
 
@@ -332,13 +334,15 @@ void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 	lastX = xpos;
 	lastY = ypos;
 
-	camera.ProcessMouseMovement(xoffset, yoffset);
+	if (!cameraLocked)
+		camera.ProcessMouseMovement(xoffset, yoffset);
 }
 
 // glfw: whenever the mouse scroll wheel scrolls, this callback is called
 // ----------------------------------------------------------------------
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 {
-	camera.ProcessMouseScroll(yoffset);
+	if (!cameraLocked)
+		camera.ProcessMouseScroll(yoffset);
 }
 
